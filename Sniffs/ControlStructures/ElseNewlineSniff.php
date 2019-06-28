805f43cc38c99992477dc11c029f1b4d928b3cc9
Nick Le Mouton <nick@noodles.net.nz>
2019-06-28 14:20:56 +1200

    Update for phpcs 3.x

--- Sniffs/ControlStructures/ElseNewlineSniff.php
+++ Sniffs/ControlStructures/ElseNewlineSniff.php
@@ -2,60 +2,45 @@
 /**
  * Checks that else/elseif statements start on a new line.
  *
- * PHP version 5
- *
- * @category PHP
- * @package  PHP_CodeSniffer
- * @author   Klaus Purer
- * @link     http://pear.php.net/package/PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  */
 
-/**
- * Checks that else/elseif statements start on a new line.
- *
- * Unfortunately we need this sniff because
- * Drupal_Sniffs_ControlStructures_ControlSignatureSniff does not
- * detect this.
- *
- * @category PHP
- * @package  PHP_CodeSniffer
- * @author   Klaus Purer
- * @link     http://pear.php.net/package/PHP_CodeSniffer
- */
-class DDC_Sniffs_ControlStructures_ElseNewlineSniff implements
-PHP_CodeSniffer_Sniff
+namespace PHP_CodeSniffer\Standards\DDC\Sniffs\ControlStructures;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
+
+class ElseNewlineSniff implements Sniff
 {
-
-
-    /**
+   /**
      * Returns an array of tokens this test wants to listen for.
      *
      * @return array
      */
     public function register()
     {
-        return array(
-                T_ELSE,
-                T_ELSEIF,
-               );
+        return [
+          T_ELSE,
+          T_ELSEIF,
+        ];
 
-    }//end register()
-
+    }
 
     /**
      * Processes this test, when one of its tokens is encountered.
      *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
      * @param int                  $stackPtr  The position of the current token in the
      *                                        stack passed in $tokens.
-     *
-     * @return void
      */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    public function process(File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
         $prevNonWhiteSpace = $phpcsFile->findPrevious(
-            PHP_CodeSniffer_Tokens::$emptyTokens,
+            Tokens::$emptyTokens,
             ($stackPtr - 1),
             null,
             true
@@ -63,13 +48,11 @@ PHP_CodeSniffer_Sniff
 
         if ($tokens[$prevNonWhiteSpace]['line'] === $tokens[$stackPtr]['line']) {
             $error = '%s must start on a new line';
-            $data  = array($tokens[$stackPtr]['content']);
+            $data  = [$tokens[$stackPtr]['content']];
             $phpcsFile->addError($error, $stackPtr, 'ElseNewline', $data);
         }
 
-    }//end process()
+    }
 
 
-}//end class
-
-?>
+}
